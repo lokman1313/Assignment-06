50a0bfc1a3838c359ff719a2e94d0d0fdb8edfdb
lokman1313 <lokmanhossenjoy13@gmail.com>
2026-04-03 23:24:42 +0600

    about section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import About from './component/abutUs/About'
 import Banner from './component/banner/Banner'
 import Navbar from './component/navbar/Navbar'
 
@@ -8,6 +9,7 @@ function App() {
     <>
      <Navbar></Navbar>
      <Banner></Banner>
+     <About></About>
     </>
   )
 }
---
+++ src/component/abutUs/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='flex justify-around p-10 text-white text-center bg-gradient-to-r from-[#4F39F6] to-[#9514FA]'>
+            <div>
+                <h2 className='text-5xl font-bold'>50K+</h2>
+                <p className='text-xl'>Active Users</p>
+            </div>
+            <div className="divider divider-horizontal before:bg-white after:bg-white"></div>
+            <div>
+                <h2 className='text-5xl font-bold'>200+</h2>
+                <p className='text-xl'>Premium Tools</p>
+            </div>
+            <div className="divider divider-horizontal before:bg-white after:bg-white"></div>
+            <div>
+                <h2 className='text-5xl font-bold'>4.9</h2>
+                <p className='text-xl'>Rating</p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
